3a4532a594193bdbfd7c4ba768d448e1cc2ca8d5
Juwoojae <juwqq1234@naver.com>
2025-08-01 04:09:56 +0900

    API

--- src/main/java/hello/helloSpring/controller/HelloController.java
+++ src/main/java/hello/helloSpring/controller/HelloController.java
@@ -1,20 +1,52 @@
 package hello.helloSpring.controller;
+
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
     //웹 애플리케이션에서 /hello라고 들어오면 아래의 메서드를 호출한다고 한다
     @GetMapping("hello")
-    public String hello(Model model){
-        model.addAttribute("data","hello!!");
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
         return "hello";
     }
+
     @GetMapping("hello-mvc")
-    public String helloMvc(@RequestParam("name") String name, Model model){
-        model.addAttribute("name",name);
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
         return "hello-template";
     }
+
+    @GetMapping("hello-string")
+    @ResponseBody//응답 바디부에 직접 넣어주겠다? 확인해보면 HTML부가 없다
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;  //hello 라는 객체를 넘겨준다  JSON 으로 반환한다
+    }
+
+    public class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+    }
+}
+
 }
